8bbbe9caac27b50b9ab25809d06b53acf1e51e4b
jorgeorejas <soy@jorgeorejas.com>
2021-05-22 18:28:46 +0200

    Tengo Miedo

--- components/Example.js
+++
@@ -1,8 +0,0 @@
-
-export default function Example() {
-  return (
-    <div className="h-full bg-white">
-      <iframe src="https://adivias.vercel.app" className="h-full pt-8"></iframe>
-    </div>
-  );
-}
--- components/icons/Logo.js
+++
@@ -1,9 +0,0 @@
-const Logo = ({ className = '', ...props }) => (
-  <>
-    <span className="text-base text-2xl font-semibold tracking-wide">
-      Adivi<span className="font-extrabold text-blue">.</span>as
-    </span>
-  </>
-);
-
-export default Logo;
